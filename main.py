2317e704a2fe0c274146807c4c701158b1db59de
ShaikaJar <ShaikaJar@users.noreply.github.com>
2022-06-29 11:45:48 +0200

    Added __main__ to main.py

--- main.py
+++ main.py
@@ -17,37 +17,36 @@ def ping():
             timing = line.split('time=')[-1].split('Zeit=')[-1].split(' ms')[0]
             return timing
 
+if __name__ == '__main__':
+    logging.basicConfig(filename="ping.log", level=logging.DEBUG, format='%(asctime)s;%(message)s',
+                        datefmt="%d.%m.%Y %H:%M:%S", filemode='a+')
+    while True:
+        t = ping()
+        if t == -1:
+            logging.error(2000)
+        else:
+            logging.debug(t)
+        print("Zeit", t, "ms")
 
-logging.basicConfig(filename="ping.log", level=logging.DEBUG, format='%(asctime)s;%(message)s', datefmt="%d.%m.%Y %H:%M:%S", filemode='a+')
+        lines = []
+        with open("ping.log") as file:
+            content = file.read()
+            lines = content.split("\n")
+        if len(lines) > 15000:
+            while len(lines) > 15000:
+                if not os.path.isdir("backup"):
+                    os.mkdir("backup")
+                date = parser.parse(lines[10000].split(";")[0], dayfirst=True)
+                print("Backing up")
+                oldLines = lines[:10000]
+                newLines = lines[10000:]
+                with open("./backup/ping_backup_" + date.strftime("%d.%m.%Y_%H-%M-%S") + ".log", 'w') as newFile:
+                    for line in oldLines:
+                        newFile.write(line + "\n")
+                lines = newLines
 
-if not os.path.isdir("backup"):
-    os.mkdir("backup")
+            with open("ping.log", 'w') as file:
+                for line in lines:
+                    file.write(line + "\n")
 
-while True:
-    t = ping()
-    if t == -1:
-        logging.error(2000)
-    else:
-        logging.debug(t)
-    print("Zeit", t, "ms")
-
-    lines = []
-    with open("ping.log") as file:
-        content = file.read()
-        lines = content.split("\n")
-    if len(lines) > 15000:
-        while len(lines) > 15000:
-            date = parser.parse(lines[10000].split(";")[0], dayfirst=True)
-            print("Backing up")
-            oldLines = lines[:10000]
-            newLines = lines[10000:]
-            with open("./backup/ping_backup_"+date.strftime("%d.%m.%Y_%H-%M-%S")+".log", 'w') as newFile:
-                for line in oldLines:
-                    newFile.write(line+"\n")
-            lines = newLines
-
-        with open("ping.log", 'w') as file:
-            for line in lines:
-                file.write(line+"\n")
-
-    time.sleep(10)
+        time.sleep(10)
